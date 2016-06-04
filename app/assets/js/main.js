4375b667ebd4af24fd5e91ecc80172b3ea39a27e
tamilarasan <tamilarasanc@kreatio.com>
2016-06-04 21:04:14 +0530

    june 4th changes 2

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -2,37 +2,34 @@
 $(document).ready(function(e) {
     $("button#submit").click(function (e) {
         e.preventDefault();
-        alert('1');
 
         if( $('#name').val() == "" || $('#email').val() == "" || $('#mobile').val() == ""  || $('#comments').val() == "")
         {
-            alert('Fill The Fields')
+            $( "#name" ).addClass( "myClass");
+            $( "#email" ).addClass( "myClass");
+            $( "#mobile" ).addClass( "myClass");
+            $( "#comments" ).addClass( "myClass");
             e.preventDefault();
         }
        else{
-            var formData = new FormData($(this)[0]);
+            var url = "/user/contact"; // the script where you handle the form input.
+
             $.ajax({
-                url: 'user/contact',
-                type: 'POST',
-                data: formData,
-                async: false,
+                type: "POST",
+                url: url,
+                data: $("#data4").serialize(), // serializes the form's elements.
+
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
                 },
                 success: function (data) {
-                    $('#coursework_list').html(data);
-                    $('#course_work_school_class_id').val("");
-                    $('#course_work_school_section_id').val("");
-                    $('#digital_asset_folder_id').val("");
-                    $('#course_work_file').val("");
 
-                },
-                cache: false,
-                contentType: false,
-                processData: false
+
+                    $('.list').html(data);
+
+
+                }
             });
-
-            return false;
         }
     });
 });
